f461040bf643921c4f589677b7da08e1de6ef9ac
John Weaver <john@pledge4code.com>
2011-08-21 07:08:22 -0700

    Misc changes.

---
+++ classifieds/templatetags/classifieds.py
@@ -0,0 +1,2 @@
+
+# TODO thumbnail related tags (show first thumbnail, show all thumbnails, get count of thumbnails), ad location output tag,  checkout progress indicator output (Select, Post, Review, Checkout), 
